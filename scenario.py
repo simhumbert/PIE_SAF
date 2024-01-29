5f299c3a2f646b3eb8ec3ae70f528491ae891a97
Victor Colombet <victorcolombet@gmail.com>
2024-01-29 13:56:45 +0100

    Modif scenario

--- scenario.py
+++ scenario.py
@@ -20,15 +20,7 @@ incorpo_2030 = 0.50           # *100 %
 incorpo_saf = [incorpo_2023, incorpo_2024, incorpo_2025, incorpo_2026, incorpo_2027, incorpo_2028, incorpo_2029, incorpo_2030]
 
 
-prix_kero_saf_an, prix_carbone_kero_saf, prix_saf_an, allowances_extra_an, prix_ap_allowances_an, prix_kero_an, prix_carbone_kero_an = calculs_with_saf(nbr_annee,
-                                                                                                                                                        volume_carbu_an,
-                                                                                                                                                        incorpo_saf,
-                                                                                                                                                        allowance_free_2023,
-                                                                                                                                                        quota_eu,
-                                                                                                                                                        carbonprice,
-                                                                                                                                                        price_saf,
-                                                                                                                                                        price_kero,
-                                                                                                                                                        allowance_SAF)
+C_MP_k, C_CO2_k, C_MP_SAF, R_UE, C_MP_k0, C_CO2_k0 = calculs_with_saf(nbr_annee, volume_carbu_an,incorpo_saf, allowance_free_2023, quota_eu, carbonprice, price_saf, price_kero, allowance_SAF)
 
 
 """Graphique :"""
@@ -40,12 +32,12 @@ labels = ['Kérosène',
           'Prix Final',
           'Scénario NO SAF']
 
-data = np.array([np.round(prix_kero_saf_an/1000000,2),
-                 np.round(prix_carbone_kero_saf/1000000,2),
-                 np.round(prix_saf_an/1000000,2),
-                 np.round(allowances_extra_an/1000000,2),
-                 np.round((prix_ap_allowances_an+prix_carbone_kero_saf)/1000000,2),
-                 np.round((prix_carbone_kero_an + prix_kero_an)/1000000,2)])
+data = np.array([np.round(C_MP_k/1000000,2),
+                 np.round(C_CO2_k/1000000,2),
+                 np.round(C_MP_SAF/1000000,2),
+                 np.round(R_UE/1000000,2),
+                 np.round((C_MP_k+C_CO2_k+C_MP_SAF+R_UE)/1000000,2),
+                 np.round((C_MP_k0 + C_CO2_k0)/1000000,2)])
 
 if __name__ == '__main__':
     
