2031f1034c9973726a910c1216d710a61c3510e3
paulbardon <paulbardon0512@gmail.com>
2024-01-29 13:55:30 +0100

    Ajout d'un taux de croissance dans la fonction fleet_carbu - Paul

--- utils/fleet_carbu.py
+++ utils/fleet_carbu.py
@@ -8,6 +8,7 @@ def fleet_carbu(annee_start, annee_end,       # int : Années de début et fin d
                 nbr_new_start,                # int : nombre de nouveaux modèles au départ (donnée)
                 replacement_nbr,              # int : nombre d'avions remplacés annuellement (hypothèse)
                 em_reduction_rate,            # float : pourcentage de réduction d'émissions des nouveaux modèles (hypothèse)
+                nbr_add_annual                # int : augmentation annuelle du nombre total d'appareils (hypothèse)
                 ):
     
     
@@ -16,14 +17,18 @@ def fleet_carbu(annee_start, annee_end,       # int : Années de début et fin d
     CO2_em[0] = CO2_em_start
     nbr_old = nbr_old_start
     nbr_new = nbr_new_start
-    CO2_em_old = CO2_em_start*nbr_old/(nbr_old+nbr_new*(1-em_reduction_rate))
-    CO2_em_new = CO2_em_start*nbr_new*(1-em_reduction_rate)/(nbr_old+nbr_new*(1-em_reduction_rate))
+    em_factor_old = (CO2_em_start*nbr_old/(nbr_old+nbr_new*(1-em_reduction_rate)))/nbr_old
+    em_factor_new = (CO2_em_start*nbr_new*(1-em_reduction_rate)/(nbr_old+nbr_new*(1-em_reduction_rate)))/nbr_new
     
     for i in range(1, nbr_annee):
-        CO2_em_old = CO2_em_old - replacement_nbr*CO2_em_old/nbr_old
-        CO2_em_new = CO2_em_new + replacement_nbr*CO2_em_new/nbr_new
         nbr_old = max(nbr_old-replacement_nbr, 0)
-        nbr_new = nbr_new+replacement_nbr
+        nbr_new = nbr_new+replacement_nbr+nbr_add_annual
+        CO2_em_old = nbr_old*em_factor_old
+        CO2_em_new = nbr_new*em_factor_new
+        
+        if nbr_old=0:
+            replacement_nbr = 0
+        
         CO2_em[i] = CO2_em_old + CO2_em_new
     
     volume_carbu_an = CO2_em * 1000000/(nrj_volum_kero * core_lca_kero)
